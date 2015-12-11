089c99defbdfb9222ccfa37f634586a3453204b0
Jimmy Lv <jinglv@thoughtworks.com>
2015-12-11 16:16:47 +0800

    #1 change to use ModelAndView for meaningful sense.

--- src/main/java/com/thoughtworks/jimmy/controller/BookShelfController.java
+++ src/main/java/com/thoughtworks/jimmy/controller/BookShelfController.java
@@ -3,10 +3,11 @@ package com.thoughtworks.jimmy.controller;
 import com.thoughtworks.jimmy.model.Book;
 import com.thoughtworks.jimmy.service.BookService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BookShelfController {
@@ -14,32 +15,35 @@ public class BookShelfController {
     private BookService bookService = new BookService();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String list(Model model){
-        model.addAttribute("books", bookService.findAll());
-        return "books";
+    public ModelAndView list() {
+        ModelMap model = new ModelMap();
+        model.put("books", bookService.findAll());
+        return new ModelAndView("books", model);
     }
 
     @RequestMapping("book/{isbn}")
-    public String showProduct(@PathVariable String isbn, Model model){
-        model.addAttribute("book", BookService.findOne(isbn));
-        return "book";
+    public ModelAndView showProduct(@PathVariable String isbn) {
+        ModelMap model = new ModelMap();
+        model.put("book", BookService.findOne(isbn));
+        return new ModelAndView("book", model);
     }
 
     @RequestMapping("book/edit/{isbn}")
-    public String edit(@PathVariable String isbn, Model model){
-        model.addAttribute("book", BookService.findOne(isbn));
-        return "newBook";
+    public ModelAndView edit(@PathVariable String isbn) {
+        ModelMap model = new ModelMap();
+        model.put("book", BookService.findOne(isbn));
+        return new ModelAndView("newBook", model);
     }
 
     @RequestMapping("book/new")
-    public String newProduct(Model model){
-        model.addAttribute("book", new Book());
-        return "newBook";
+    public ModelAndView newProduct() {
+        ModelMap model = new ModelMap();
+        model.put("book", new Book());
+        return new ModelAndView("newBook", model);
     }
 
     @RequestMapping(value = "book", method = RequestMethod.POST)
-    public String saveProduct(Book book){
-
+    public String saveProduct(Book book) {
         bookService.save(book);
         return "redirect:/book/" + book.getIsbn();
     }
